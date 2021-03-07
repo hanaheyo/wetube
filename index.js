4ebcd3ff2e4eaf645378c24e86c2744c6aeefdf3
hanaheyo <violets5618@gmail.com>
2021-03-07 17:48:33 +0900

    #2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -11,8 +11,17 @@ const handleHome = (req, res) =>
 
 const handleProfile = (req, res) =>  res.send("You are on my profile");
 
+const betweenHome = (req, res, next) => {
+    console.log("I'm between");
+    next(); // middleware 함수인 betweenHome 호출
+}
+
+// 위에서 아래 순서로 실행되기 때문에, 여기에다가 놓으면 주소가 어디든 항상 실행됨!
+app.use(betweenHome)
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
+ 
