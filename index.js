f9e35b02dea2c2b115913b637c94832693ab1488
hanaheyo <violets5618@gmail.com>
2021-03-13 21:58:23 +0900

    #2.7 Express Core: Middlewares part Two -B

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -14,6 +16,10 @@ const handleHome = (req, res) =>
 
 const handleProfile = (req, res) =>  res.send("You are on my profile");
 
+app.use(cookieParser());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(helmet());
 app.use(morgan("dev"));
 
 app.use(helmet());
