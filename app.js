88e311feb5fd9a0f414058c3efbd16f3a1deb934
hanaheyo <violets5618@gmail.com>
2021-03-14 17:57:30 +0900

    #2.9 MVC Pattern part One

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router" // default로 export하지 않았기 때문에 이렇게  import하는 것
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-const handleHome = (req, res) =>  res.send("Hello from home ");     // req: request object / res: response object
-
-const handleProfile = (req, res) =>  res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,10 +15,9 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter)
+// Seperate Url
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
  
 export default app; // 누군가 내 파일을 import 하면, app object를 주겠다는 의미
